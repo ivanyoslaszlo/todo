33875e10633907d638f02ecbe72e73514d0262ef
IvanyosLaszlo <ivanyoslaszlo25@gmail.com>
2025-09-15 17:52:47 +0200

    név váltás AdminController-re

--- src/main/java/com/example/demo/controllers/AdminController.java
+++ src/main/java/com/example/demo/controllers/AdminController.java
@@ -5,18 +5,17 @@ import com.example.demo.respository.Repository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 
-public class UserController {
+public class AdminController {
 
     private final Repository userRepository;
 
-    public UserController(Repository userRepository) {
+    public AdminController(Repository userRepository) {
         this.userRepository = userRepository;
     }
 
